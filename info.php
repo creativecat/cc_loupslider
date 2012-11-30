0179355f4624c09020c5129d0f1851d88df3c466
Matthias Glienke <support@creativecat.de>
2012-11-30 02:11:15 +0100

    Adding protection

--- info.php
+++ info.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ *  @template		cc_loupslider
+ *  @version		see info.php of this template
+ *  @author			Matthias Glienke, creativecat
+ *  @copyright		2012 Matthias Glienke
+ *  @license		Copyright by Matthias Glienke, creativecat
+ *  @license terms	see info.php of this module
+ *  @platform		see info.php of this module
+ *  @requirements	PHP 5.2.x and higher
+ */
+
+/* -------------------------------------------------------- */
+// Must include code to stop this file being accessed directly
+if(!defined('WB_PATH')) {
+	require_once(dirname(dirname(__FILE__)).'/framework/globalExceptionHandler.php');
+	throw new IllegalFileException();
+}
+/* -------------------------------------------------------- */
+
 $module_directory	= 'cc_loupslider';
 $module_name		= 'cc_loupslider';
 $module_function	= 'page';
